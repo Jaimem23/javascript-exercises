caa46838f9d07a9645fce71601581d1df321a162
Jaimem23 <140969227+Jaimem23@users.noreply.github.com>
2023-08-22 20:10:27 +0200

    Exercises 4 and 5 completed
    4 was horrible

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringChars = [];
+    let joinedString = "";
+    for (i = 0; i < string.length;i++ ){
+        stringChars = stringChars.concat(string[i]);
+    }
+    stringChars.reverse();
+    for(i= 0;stringChars.length > i;i++){
+        joinedString += stringChars[i];
+        
+    }
+    
+    return joinedString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+   let addition = 0; 
+   //See if it's negative
+    if(num1 < 0 || num2 < 0||typeof num1 != "number"||typeof num2 != "number") return "ERROR";
+    
+    if(num1 < num2){
+        
+        for( ;num1 <= num2; num1++){
+            addition += num1;
+        }
+        return addition;
+    }
+    else{
+        for( ;num1 >= num2; num1--){
+            addition += num1;
+        }
+        return addition;
+    }
+   
 };
 
 // Do not edit below this line
