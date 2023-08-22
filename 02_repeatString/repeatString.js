91d8cdd6bdcc6ee5b0c23136e82041a310e0d6f8
Jaimem23 <140969227+Jaimem23@users.noreply.github.com>
2023-08-22 16:59:12 +0200

    First two exercises complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let stringRepeated ="";
+    if(num < 0)  return "ERROR";
+    for(i = 0;i < num; i++){
+         stringRepeated = stringRepeated.concat(string);
+    }
+    
+    return stringRepeated;
 };
 
 // Do not edit below this line
